97274fc812202d3601f602f4268e64465cbb72bc
Aruns760 <arunajay9790@gmail.com>
2025-06-18 12:03:04 +0530

    Resolved merge conflict in script.js

--- js/script.js
+++ js/script.js
@@ -10,18 +10,10 @@ header.addEventListener('click', () => {
 document.querySelector('footer').innerHTML += ` 
   <p>© ${new Date().getFullYear()} - Arun</p>`;
 
-<<<<<<< HEAD:js/script.js
-// Add version number to force update
-const version = "1.0.2"; // ← Change this number after edits
-=======
-// dark and day mode toggle
+// Dark Mode Toggle
 const toggle = document.getElementById("darkModeToggle");
->>>>>>> 3348e5c4b5292cab72da325fd2674f38b6b10fbf:script.js
-
-document.getElementById("darkModeToggle").addEventListener("click", () => {
+toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
-  // Update button text
-  const toggle = document.getElementById("darkModeToggle");
   toggle.textContent = document.body.classList.contains("dark-mode") 
     ? "☀️ Light Mode" 
     : "🌙 Dark Mode";
